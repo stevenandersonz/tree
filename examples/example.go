edf1257a99b7a0057c8a7ecfd6180382a0240a5f
Steven Anderson <sdandersonz@icloud.com>
2021-12-22 23:56:42 -0800

    getting somewhre

--- examples/example.go
+++ examples/example.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"syscall/js"
-
 	"github.com/stevenandersonz/tree"
 )
 
@@ -11,6 +10,14 @@ func printHello (this js.Value, args []js.Value) interface {} { return true }
 
 
 func main() {
+	doc := tree.GetDocument()
+	console := tree.GetConsole()
+	console.Log(tree.GetNodeType(doc.JSValue))
+	mainSection := doc.GetElementById("tree-main")
+	p := doc.CreateHTMLElement("p")
+	text := doc.CreateHTMLTextNode("Hello from tree")
+	p.AppendChild(text)
+	mainSection.AppendChild(p)
 	api := tree.NewAPI("myAPI")
 	api.Add("printHello",printHello)
 	api.Publish()
